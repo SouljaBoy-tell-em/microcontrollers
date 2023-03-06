63d77d6f8db5b3e2de7a2d026ff762ebf60ac7d0
hellprogmaker <60592559+hellprogmaker@users.noreply.github.com>
2023-03-06 12:25:01 +0300

    Add files via upload

---
+++ 01_blinkled/bits.h
@@ -0,0 +1,13 @@
+#ifndef __HEADER_BITS_H__
+#define __HEADER_BITS_H__
+
+
+//--------
+// MACROS
+//--------
+
+#define MODIFY_REG(reg, modifymask, value) (*(reg) = ((*(reg) & (~(modifymask))) | (value & modifymask)))
+#define SET_BIT_ON(reg, bit)  (*(reg) |= 1U << (bit))       // set bit in register to 1
+#define SET_BIT_OFF(reg, bit) (*(reg) &= (~(1U << (bit))))  // set bit in register to 0
+
+#endif
