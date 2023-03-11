e53b8aec879ec271b0ec13aa31ec971e148066e6
hellprogmaker <60592559+hellprogmaker@users.noreply.github.com>
2023-03-11 16:44:38 +0300

    Add files via upload

---
+++ reaction.c
@@ -0,0 +1,47 @@
+#include "header_reaction.h"
+#include "header_seg7.h"
+
+#include "bits.h"
+#include "registers.h"
+#include "gpio.h"
+#include "seg7.h"
+#include "reaction.h"
+
+
+//------
+// Main
+//------
+
+int main() {
+
+    // Init display rendering:
+    Seg7Display seg7        = { .display = 0, .number = 0};
+    bool active             =                        false;
+    bool button_was_pressed =                        false;
+    uint32_t saturation     =                           0U;
+    uint32_t tick           =                           0U;
+
+    board_clocking_init();
+    board_gpio_init();
+
+    while (true) {
+
+        // Update button state:
+        GPIOA_IDR_GET_VALUE_PA0_INPUT(active, GPIOA_IDR, 0U);
+
+        // Contact bounce prevention:
+        TURN_ON_CONTACT_DEBOUNCER(active, saturation, button_was_pressed);
+
+        // Update display state, while button wasn't pressed
+        // not equal UPLIMIT:
+        UPDATE_DISPLAY_STATE_TO_UPLIM(button_was_pressed, tick, 10, seg7, UPLIMIT);
+
+        // Render display state:
+        SEG7_set_number_quarter(&seg7, tick);
+        SEG7_push_display_state_to_mc(&seg7);
+
+        // Adjust ticks every ms:
+        to_get_more_accuracy_pay();
+        tick++;
+    }
+}
